e3a6a4922d08b30d10a6f99b27beb73d29793046
carolinagy <157058601+carolinagy@users.noreply.github.com>
2026-03-30 23:07:29 -0300

    Add files via upload

---
+++ Netflix/catalogo/js/data.js
@@ -0,0 +1,41 @@
+export const categories = [
+    {
+        title: "Favoritos",
+        items: [
+            {
+                img: "https://s2-g1.glbimg.com/pWn_01YOXVuqLIETj5DvLk1w2Oc=/0x0:1280x720/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2024/v/o/FBD7z9ThqohfCpsuxSXQ/invencivel.jpeg", top10: true, badge: "Clássico", badgeColor: "red", progress: 50,
+                youtube: "https://www.youtube.com/watch?v=QvnTluSzq8s"
+            },
+            {
+                img: "https://m.media-amazon.com/images/S/pv-target-images/c82ef4ff7dfbb0b5d9915dab0e1aaeb02837ef4e693ab768bd93d261b1d8baae.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=yNVF6eIEQII"
+            },
+            {
+                img: "https://s2-techtudo.glbimg.com/QDIYAODrGD6Wx6lO5ALZoplvQJw=/0x0:1200x800/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/w/q/GQtAzUSC23yGNHAaG36A/8641cd99-093f-4f85-9a26-388494eb3a7f.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=W6TVcu1kznk"
+            },
+            {
+                img: "https://images.paramount.tech/uri/mgid:arc:imageassetref:shared.southpark.br:bc418d66-7342-11ea-a59c-0a7527021758?quality=0.7&gen=ntrn", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=B6xooKpoc4g"
+            },
+        ]
+    },
+    {
+        title: "Séries",
+        items: [
+            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+        ]
+    },
+    {
+        title: "Para maratonar",
+        items: [
+            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+        ]
+    }
+];
